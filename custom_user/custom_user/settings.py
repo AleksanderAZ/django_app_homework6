49edb92a71ef06937cc39874e085417f539e2421
AleksanderAZ <aleksanderaz@gmail.com>
2025-03-16 15:59:05 +0200

    Add homework 7 (Template tags)

--- custom_user/custom_user/settings.py
+++ custom_user/custom_user/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
+    "permissions",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = "custom_user.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'permissions'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- custom_user/permissions/views.py
+++ custom_user/permissions/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import get_user_model
 from django.contrib import messages
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models  import ContentType
+
+User = get_user_model()
 
 @login_required
 @permission_required('accounts.can_view_profiles', '/')
@@ -12,5 +16,38 @@ def restricted_page(request):
 def su_only(request):
     if not request.user.is_superuser:
         messages.error(request, "Тільки для Адміністратора")
+    render(request, 'permissions/su_page.html', {'user': request.user})
 
+@login_required
+def list_users(request):
+    if not request.user.is_superuser:
+        messages.error(request, "Тільки для Адміністратора")
+        return redirect('home')
+    users = User.objects.all().exclude(id=request.user.id)
+    return render(request, 'permissions/user_list.html', {'users': users})
+
+@login_required
+def manage_permissions(request, user_id):
+    if not request.user.is_superuser:
+        messages.error(request, "Тільки для Адміністратора")
+        return redirect('home')
+    
+    user = get_object_or_404(User, id=user_id)
+
+    content_type = ContentType.objects.get(app_label='accounts', model='customuser')
+    user_model_permissions = Permission.objects.filter(content_type=content_type)
+
+    if request.method == 'POST':
+        print("!!!!!!!!!!!!!!!!!!!!!!!")
+        selected_permissions = request.POST.getlist('permissions')
+        user.user_permissions.set(selected_permissions)
+        messages.success(request, 'Дозволи надані')
+        return redirect('manage_permissions', user_id=user.id)
+    
+    context = {
+        'user': user,
+        'permissions': user_model_permissions,
+        'user_permissions': user.user_permissions.all()
+    }
+    return render(request, 'permissions/user_permissions.html', context)
 
